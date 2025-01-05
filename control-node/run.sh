c49e725c4898447c5b614eaa19f6e84b8e0c1c45
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-01-05 16:14:35 +0000

    removed container names to allow easy scaling

--- control-node/run.sh
+++ control-node/run.sh
@@ -4,7 +4,6 @@
 ssh-keygen -t rsa -b 4096 -f /root/.ssh/id_rsa -N  ""
 
 # Copy public key to shared-files, where it can be accessed by other containers
-mkdir /shared-files
 cp /root/.ssh/id_rsa.pub /shared-files/control_node_key.pub
 
 # Keep container running
--- host/run.sh
+++ host/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Waits for master key to exist
 while [ ! -f /shared-files/control_node_key.pub ]; do
@@ -8,7 +8,12 @@ done
 
 # Add control node key to authorized_keys
 mkdir -p ~/.ssh
-cat /shared-files/control_node_key.pub >> ~/.ssh/authorized_keys
+cat /shared-files/control_node_key.pub > ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
+
+# Generate SSH host keys if they do not exist
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    ssh-keygen -A
+fi
 
 /usr/sbin/sshd -D
